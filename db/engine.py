7c3bdfa98f1f6f510c88430b77c8fa4c29c296a5
Ashkan Khani <drashkankhani@gmail.com>
2024-07-30 22:45:25 +0330

    add cors middleware and some changes

--- db/engine.py
+++ db/engine.py
@@ -1,4 +1,4 @@
-from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine, AsyncSession
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeBase, MappedAsDataclass
 
 SQLALCHEMY_DATABASE_URL = "sqlite+aiosqlite:///./Analysis.db"
